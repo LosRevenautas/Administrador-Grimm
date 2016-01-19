24705e6540c74862523a11730ff5d69f9efb780d
Merlin <Merlin@WIN-QKJ6R8J47TJ>
2016-01-18 21:08:52 -0300

    Crecion metodos y atributos clase usuario

--- Administrador/src/administrador/Entidades/EntidadesAbstractas/Usuario.java
+++ Administrador/src/administrador/Entidades/EntidadesAbstractas/Usuario.java
@@ -5,10 +5,67 @@
  */
 package administrador.Entidades.EntidadesAbstractas;
 
+import java.util.Calendar;
+
 /**
  *
  * @author Merlin
  */
 public abstract class Usuario extends EntidadAbstracta{
+    private String nombre;
+    private int telefono;
+    private String direccion;
+    private Calendar inicioContrato;
+    private Calendar finContrato;
+    private boolean[] permisos;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public Calendar getInicioContrato() {
+        return inicioContrato;
+    }
+
+    public void setInicioContrato(Calendar inicioContrato) {
+        this.inicioContrato = inicioContrato;
+    }
+
+    public Calendar getFinContrato() {
+        return finContrato;
+    }
+
+    public void setFinContrato(Calendar finContrato) {
+        this.finContrato = finContrato;
+    }
+
+    public boolean[] getPermisos() {
+        return permisos;
+    }
+
+    public void setPermisos(boolean[] permisos) {
+        this.permisos = permisos;
+    }
+    
     
 }
