ef59b2c7ba948c23742448a2481f652597a9b0b3
Merlin <Merlin@WIN-QKJ6R8J47TJ>
2016-01-18 21:06:51 -0300

    Style refactoring

--- Administrador/src/administrador/Utils/DBAdmin.java
+++ Administrador/src/administrador/Utils/DBAdmin.java
@@ -17,88 +17,87 @@ import administrador.Entidades.Retiro;
  * @author Merlin
  */
 public class DBAdmin {
-    
-    public Usuario isValido(String user, String pass){
-    Usuario logueado = new Administrador(); //buscar usuario logueado.
+
+    public Usuario isValido(String user, String pass) {
+        Usuario logueado = new Administrador(); //buscar usuario logueado.
         return logueado;
     }
-    
-    
-    public void setNumMesas(byte mesas){
+
+    public void setNumMesas(byte mesas) {
         //place holder
     }
-    
-    public byte getNumMesas(){
-    return 10;
+
+    public byte getNumMesas() {
+        return 10;
     }
-    
-    public Pedido[] getPedidosPendientes(){
-    Pedido[] pendientes = null;
-    return pendientes;
+
+    public Pedido[] getPedidosPendientes() {
+        Pedido[] pendientes = null;
+        return pendientes;
     }
-    
-    public Pedido[] getPedidos(){
-    Pedido[] pedidos = null;
-    return pedidos;
+
+    public Pedido[] getPedidos() {
+        Pedido[] pedidos = null;
+        return pedidos;
     }
-    
+
     //Guarda el pedido en la DB y devuelve el ID generado para el pedido
-    public int addPedido(Pedido pedido){
-    return 0;
-    }
-    
-    public void changePedido(Pedido pedido){
-        //place holder
-    }
-    
-    public Retiro[] getRetiros(){
-    Retiro[] listaRetiros = null;
-    return listaRetiros;
-    }
-    
-    public void addRetiro(Retiro retiro){
-        //place holder
-    }
-    
-    public void changeRetiro(Retiro retiro){
-    }
-    
-    public int addProducto(Producto producto){
+    public int addPedido(Pedido pedido) {
         return 0;
     }
-    
-    public void changeProducto(Producto producto){
+
+    public void changePedido(Pedido pedido) {
         //place holder
     }
-    
-    public Producto[] getProductos(){
+
+    public Retiro[] getRetiros() {
+        Retiro[] listaRetiros = null;
+        return listaRetiros;
+    }
+
+    public void addRetiro(Retiro retiro) {
+        //place holder
+    }
+
+    public void changeRetiro(Retiro retiro) {
+    }
+
+    public int addProducto(Producto producto) {
+        return 0;
+    }
+
+    public void changeProducto(Producto producto) {
+        //place holder
+    }
+
+    public Producto[] getProductos() {
         Producto[] productosLista = null;
         return productosLista;
     }
-    
-    public Usuario[] getUsuarios(){
-    Usuario[] usuarios = null;
-    return usuarios;
+
+    public Usuario[] getUsuarios() {
+        Usuario[] usuarios = null;
+        return usuarios;
     }
-    
-    public void addUsuario(Usuario usuario){
-    //place holder
+
+    public void addUsuario(Usuario usuario) {
+        //place holder
     }
-    
-    public void changeUsuario(Usuario usuario){
-    //place holder
+
+    public void changeUsuario(Usuario usuario) {
+        //place holder
     }
-    
-    public Receta[] getRecetas(){
-    Receta[] recetas = null;
-    return recetas;
+
+    public Receta[] getRecetas() {
+        Receta[] recetas = null;
+        return recetas;
     }
-    
-    public void addReceta(Receta receta){
-    //place holder
+
+    public void addReceta(Receta receta) {
+        //place holder
     }
-    
-    public void changeReceta(Receta receta){
-    //place holder
+
+    public void changeReceta(Receta receta) {
+        //place holder
     }
 }
