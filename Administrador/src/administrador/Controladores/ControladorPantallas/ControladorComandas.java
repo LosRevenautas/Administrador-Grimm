788799cb7a0b74df043db46b892809a0439a94e6
Merlin <Merlin@WIN-QKJ6R8J47TJ>
2016-01-19 00:53:13 -0300

    Merge origin/master
    Conflicts:
    	Administrador/nbproject/project.properties

--- Administrador/src/administrador/Controladores/ControladorPantallas/ControladorComandas.java
+++ Administrador/src/administrador/Controladores/ControladorPantallas/ControladorComandas.java
@@ -5,13 +5,58 @@
  */
 package administrador.Controladores.ControladorPantallas;
 
-import administrador.Controladores.ControladoresAbstractos.ControladorModulosAbstracto;
-
+import administrador.Controladores.ControladoresAbstractos.ControladorPantallaAbstracto;
+import administrador.Pantallas.PantallaComandas;
+import administrador.Pantallas.PantallasAbstractas.PantallaAbstracta;
+import administrador.Utils.ReadPropertie;
 
 /**
  *
  * @author Merlin
  */
-public class ControladorComandas extends ControladorModulosAbstracto{
-    
+public class ControladorComandas extends ControladorPantallaAbstracto {
+
+    private ReadPropertie reader = new ReadPropertie();
+    private PantallaAbstracta comandas;
+    private int numMesas = 0;
+    private javax.swing.JPanel pnlMesas;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+
+    public ControladorComandas(PantallaComandas comandas) {
+        this.comandas = comandas;
+        numMesas = Integer.valueOf(reader.getPropertie("numMesas"));
+    }
+
+    public void buildMesas() {
+        pnlMesas = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        javax.swing.GroupLayout pnelMesasLayout = new javax.swing.GroupLayout(pnlMesas);
+        pnlMesas.setLayout(pnelMesasLayout);
+        pnelMesasLayout.setHorizontalGroup(pnelMesasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(pnelMesasLayout.createSequentialGroup()
+                        .addGroup(pnelMesasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                .addGroup(pnelMesasLayout.createSequentialGroup()
+                                        .addComponent(jButton1)
+                                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                                        .addComponent(jButton2))
+                                .addComponent(jButton3))
+                        .addGap(0, 224, Short.MAX_VALUE))
+        );
+        pnelMesasLayout.setVerticalGroup(pnelMesasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(pnelMesasLayout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(pnelMesasLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                                .addComponent(jButton1)
+                                .addComponent(jButton2))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jButton3)
+                        .addContainerGap(215, Short.MAX_VALUE))
+        );
+        comandas.add(pnlMesas, 1);
+        comandas.pack();
+    }
 }
