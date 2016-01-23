1a061f0d3ff35428ebfb39e83914eac777a51dcd
Merlin <Merlin@WIN-QKJ6R8J47TJ>
2016-01-23 19:36:31 -0300

    Continuando con la clase controladorComandas y el metodo onClick

---
+++ Administrador/src/administrador/Entidades/EntidadesAbstractas/Contenedor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package administrador.Entidades.EntidadesAbstractas;
+
+import administrador.Entidades.Mesa;
+import administrador.Entidades.Pedido;
+import administrador.Entidades.Producto;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Merlin
+ */
+public interface Contenedor {
+    ArrayList<Mesa> LISTMESA = new ArrayList<>();
+    ArrayList<Pedido> LISTPEDIDO = new ArrayList<>();
+    ArrayList<Producto> LISTPRODUCTO = new ArrayList<>();
+}
